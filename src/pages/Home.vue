02d54cdb722b8e0b7dfcb154df5235c567b597be
SeungjunLee <seungjun.dev@gmail.com>
2022-04-02 17:42:36 -0700

    created home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page main-container">This is home</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="css">
+.main-container {
+  width: 100%;
+  min-height: 100vh;
+  background: #355c7d;
+  background: -webkit-linear-gradient(to right, #c06c84, #6c5b7b, #355c7d);
+  background: linear-gradient(to right, #c06c84, #6c5b7b, #355c7d);
+}
+</style>
